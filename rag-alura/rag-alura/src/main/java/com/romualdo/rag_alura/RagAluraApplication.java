3d58bea50d462a2afa03ce742bb4a5dbd5b58336
junior <jromualdo3@hotmail.com>
2026-08-21 12:29:39 -0300

    feat(rag app and test): add rag application and test.

---
+++ rag-alura/rag-alura/src/main/java/com/romualdo/rag_alura/RagAluraApplication.java
@@ -0,0 +1,13 @@
+package com.romualdo.rag_alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagAluraApplication.class, args);
+	}
+
+}
